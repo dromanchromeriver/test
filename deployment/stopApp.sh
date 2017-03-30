0315cdeb54e4dbfc46d16ed3f736e9989ff2b256
System Administrator <david.roman@chromeriver.com>
2017-03-30 12:12:40 -0700

    deployment update

--- deployment/stopApp.sh
+++ deployment/stopApp.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ "ps -ae |grep node |grep -v grep|awk '{print $1}'" -gt 0 ] 
+PID=$( ( ps -ae |grep node |grep -v grep|awk '{print $1}' ) ) #check for node process id
+
+
+#if we have a process value in @var then we need to remove(kill) it
+if [ "$PID" ]; then
 	kill `ps -ae |grep node |grep -v grep|awk '{print $1}'` | exit 0
-then
 fi
-
-
