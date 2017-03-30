6e41a602028cf65ad43ec8a141dc0aafe4bb640e
System Administrator <david.roman@chromeriver.com>
2017-03-30 11:49:23 -0700

    deployment update

--- deployment/startApp.sh
+++ deployment/startApp.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#hello
 
 export NODE_PORT=1337
 npm start & exit 0
--- deployment/stopApp.sh
+++ deployment/stopApp.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-kill `ps -ae |grep node |grep -v grep|awk '{print $1}'` | exit 0
+if [ "ps -ae |grep node |grep -v grep|awk '{print $1}'" -gt 0 ] 
+	kill `ps -ae |grep node |grep -v grep|awk '{print $1}'` | exit 0
+then
+fi
+
+
